e8fde2271ad934117224249de82203ac379d833f
Nirbhay Singh <nirbhaysinghbe1@gmail.com>
2022-02-07 22:48:42 +0530

    completed win logic and some corner cases covered\

--- problem_logic.py
+++ problem_logic.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 
 board_size = 4  # it could be changed later if we want
@@ -127,6 +128,35 @@ def merge_bottom(currentBoardStatus):
 def pickRandomValue():
     return random.choice([2, 4])
 
+# This function is going to add a new value to the board in any empty spaces
+
+
+def addValue():
+    rowValue = random.randint(0, board_size-1)
+    colValue = random.randint(0, board_size-1)
+
+    # Picking the spots until we find one thats empty
+    while(not board[rowValue][colValue] == 0):
+        rowValue = random.randint(0, board_size-1)
+        colValue = random.randint(0, board_size-1)
+
+    # filling the empty space with a new value
+    board[rowValue][colValue] = pickRandomValue()
+
+
+# This functions will check whether the user has won or not
+
+def isWon():
+    for row in board:
+        if 2048 in row:
+            return True
+    return False
+
+
+# This function is going to check for losses
+def noMoves():
+    pass
+
 
 # Creating a blank board
 board = []
@@ -138,7 +168,7 @@ for i in range(board_size):
         row.append(0)
     board.append(row)
 
-# Fill two spots with random values at the start of the game
+# Filling two spots with random values at the start of the game
 numberNeeded = 2
 while(numberNeeded > 0):
 
@@ -148,13 +178,58 @@ while(numberNeeded > 0):
         board[randomRow][randomCol] = pickRandomValue()
         numberNeeded -= 1
 
-print('''This is the 2048 game ! In this game your goal is to combine values 
+print('''This is the 2048 game ! In this game your goal is to combine values
 to get the number 2048, by merging the board in different directions.
 You will have to press either  1, 2, 3 or 4 for left, right, up and down movements
 \n\nHere is the starting board for you to start playing....\n''')
 
 
 gameEnded = False
+
+# Keep asking the user for the new moves while the game is not over
+while not gameEnded:
+    move = int(input(
+        "Which direction do you want to move? Press 1 for left 2 for right 3 for up and 4 for down. "))
+  # Assuming the user entered a valid value for input
+
+    validInput = True
+
+    # creating a  new copy of the board which is going to be the same so deepcopy
+    temporaryBoard = copy.deepcopy(board)
+
+   # Finding whether the input was valid or not and use the correct move function
+    if move == 1:
+        board = merge_left(board)
+
+    elif move == 2:
+        board = merge_right(board)
+
+    elif move == 3:
+        board = merge_top(board)
+
+    elif move == 4:
+        board = merge_bottom(board)
+
+    else:
+        validInput = False
+    # if the input was invalid,the user need to enter a new input
+    if not validInput:
+        print("Kindly press valid input , please try again..")
+ # otherwise the user input was valid
+    else:
+        # Checking if the board is still equal to the temporary board which we created earlier
+        if(board == temporaryBoard):
+            # telling the user to try again
+            print("Pleas try moving in different direction")
+
+        else:
+            # checking if the user has won
+            if isWon():
+                display_board(board)
+                print("\nYou won!!!!!\n")
+                gameEnded = True
+            addValue()
+            display_board(board)
 
 
 merge_left(board)
