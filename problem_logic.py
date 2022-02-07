89a820703cc8c2f6f3f5d8ec32ef67f4efe0196c
Nirbhay Singh <nirbhaysinghbe1@gmail.com>
2022-02-07 18:00:19 +0530

    merging left is done

--- problem_logic.py
+++ problem_logic.py
@@ -10,18 +10,62 @@ def display_board(board):
         for element in row:
             if(element > maximum):
                 maximum = element
+
+    spaceLength = len(str(maximum))
     for row in board:
         current_Row = "╏ "
 
         for element in row:
             if(element == 0):
-                current_Row += (len(str(maximum))*"  "+"╏ ")
+                current_Row += (spaceLength*"  "+"╏ ")
             else:
-                current_Row += ((len(str(maximum))-len(str(element)))*"  " +
+                gap = spaceLength-len(str(element))
+                current_Row += (gap*"  " +
                                 str(element)+" ╏ ")
         print(current_Row)
-        print("-"*maximum*(board_size+1))
+        print("-"*board_size*(board_size))
     print()
 
 
+display_board(board)
+
+# This function will be used for merging one row to the left
+
+
+def mergeOneRowLeft(row):
+        # move everything to the left
+
+    for j in range(board_size-1):
+        for i in range(board_size-1, 0, -1):
+                # testing for any empty spaces,move ahead if there is
+
+            if(row[i-1] == 0):
+                row[i-1] = row[i]
+                row[i] = 0
+    # merging everything to their left side
+    for i in range(board_size-1):
+            # checking if the value of the element is same as the element ahead of it
+        if(row[i] == row[i+1]):
+                # if true then multiplying by 2 and keeping the other element as 0
+            row[i] *= 2
+            row[i+1] = 0
+
+    for i in range(board_size-1, 0, -1):
+        if(row[i-1] == 0):
+            row[i-1] = row[i]
+            row[i] = 0
+    return row
+
+
+# This function is going to merge whole board to the left side
+def merge_left(currentBoardStatus):
+        # Merge every row in the board to the left
+    for i in range(board_size):
+
+        currentBoardStatus[i] = mergeOneRowLeft(currentBoardStatus[i])
+
+    return currentBoardStatus
+
+
+merge_left(board)
 display_board(board)
--- readme.md
+++ readme.md
@@ -3,12 +3,19 @@
 ## Defining Requirements
 
 ### 1.Having a desirable board size.
+
 ### 2.Creating a Board
+
 ### 3.Displaying the board at each step
+
 ### 4.Defining the function to merge in each direction
+
 ### 5.Defining a logic for implementing the game functionality of adding in pairs.
+
 ### 6.Defining the game logic for invalid input or no possible moves
+
 ### 7. Implementing a winning logic.
+
 ### 8. Implementing a logic for loss.
 
 ## Defining Requirements
@@ -17,4 +24,8 @@
 
 ## The program needs:-
 
+## Completed Features:-
+
 #### 1. Starting with displaying and creating board
+
+#### 2. Working with game major functionality
